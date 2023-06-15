675fc3f22ccf85b6daa8354a4ef8b419730088cf
uchitosato <uchitosato@gmail.com>
2023-06-16 01:19:57 +0900

    2023-6-15 updates

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import time
 
 from selenium import webdriver
-from src.utilities.constants import MY_CONSTANT
+from src.utilities.constants import MY_CONSTANT, PASSWORD, GMAIL_ADDRESS
 from selenium.webdriver.common.keys import Keys
-
-
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 def main():
-    driver.get("https://www/python.org")
-    time.sleep(30)
-    # driver.find_element("Connect Wallet")
-    print(MY_CONSTANT)
+    driver.get("https://www.google.com/accounts/Login")
+    email_input = driver.find_element(by=By.NAME, value="identifier")
+    time.sleep(0.5)
+    email_input.send_keys(GMAIL_ADDRESS)
+    time.sleep(0.5)
+    next_button = driver.find_element(by=By.ID, value="identifierNext")
+    time.sleep(0.5)
+    next_button.click()
+    time.sleep(0.5)
+    passord_input = driver.find_element(by=By.NAME, value="Password")
+    time.sleep(0.5)
+    passord_input.send_keys(PASSWORD)
+    time.sleep(0.5)
+    password_next = driver.find_element(by=By.ID, value="passwordNext")
+    time.sleep(0.5)
+    password_next.click()
+    time.sleep(100)
+
 
 if __name__ == '__main__':
     main()
